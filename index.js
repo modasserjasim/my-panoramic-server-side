0a9b67553a898882d35bfa5d0939d7f626b54aa0
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-11-08 22:16:58 +0600

    Services data fetch using get method

--- index.js
+++ index.js
@@ -26,6 +26,50 @@ run();
 
 const serviceCollection = client.db('myPanoramic').collection('services');
 
+// get data from client and save to db
+app.post('/service', async (req, res) => {
+    try {
+        const result = await serviceCollection.insertOne(req.body);
+        console.log('Service added', result);
+        if (result.insertedId) {
+            res.send({
+                status: true,
+                message: `Your service ${req.body.title} has been Successfully added`,
+            });
+        } else {
+            res.send({
+                status: false,
+                error: "Error Occurred! Couldn't create the service."
+            })
+        }
+    } catch (error) {
+        console.log(error.name.bgRed, error.message.bold);
+        res.send({
+            success: false,
+            error: error.message
+
+        })
+    }
+})
+
+//get the services from the database
+app.get('/services', async (req, res) => {
+    try {
+        const cursor = serviceCollection.find({});
+        const services = await cursor.toArray();
+        res.send({
+            status: true,
+            services: services
+        })
+
+    } catch (error) {
+        console.log(error.name.bgRed, error.message.bold);
+        res.send({
+            status: false,
+            error: error.message
+        })
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('My Panorama App is running!');
